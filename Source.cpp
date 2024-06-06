c8668f15469095b0b3cc9c8d5dc8fb3fe3d1c040
karasb1 <bohdankaras456@gmail.com>
2024-06-06 20:05:12 +0300

    The ex3 was done.

--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int sumInRange(int a, int b, int s = 0) {
+    if (a == b) {
+        return s + a;
+    }
+    sumInRange(++a, b, s += a);
+}
 
-	return 0;
+int main() {
+    int a, b;
+    cout << "Enter the beginning of range: ";
+    cin >> a;
+    cout << "Enter end of range: ";
+    cin >> b;
+    cout << sumInRange(a, b) <<endl;
+
+    return 0;
 }
