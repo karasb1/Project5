6a9bf789de0c18689b4c7a972b14a6ae3081a04b
karasb1 <bohdankaras456@gmail.com>
2024-06-06 19:18:05 +0300

    .

--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 
-int main()
-{
 
-	return 0;
+void func_pow(int a, int b, int d, int i = 0)
+{
+    if (i < b)
+    {
+        d *= a;
+        func_pow(++i);
+    }
+}
+
+
+int main() {
+    int number, degree;
+    cout << "Enter the number: ";
+    cin >> number;
+    cout << "Enter the degree: ";
+    cin >> degree;
+    int d = number;
+    func_pow(number, degree, d);
+    return 0;
 }
