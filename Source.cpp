e505b384142186a491c329e250bc3c92fefb1102
karasb1 <bohdankaras456@gmail.com>
2024-09-14 16:45:06 +0300

    The ex6 was done.

--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 using namespace std;
 
+int h(int number)
+{
+	if (number > 0)
+	{
+		return true;
+	}
+	else if (number < 0)
+	{
+		return false;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+
 int main()
 {
-
+	int number;
+	cin >> number;
+	cout << h(number) << endl;
 	return 0;
 }
