c5a61d982112fc127a6169f1f49400957a0a9981
karasb1 <bohdankaras456@gmail.com>
2024-09-14 14:38:14 +0300

    The ex2 was done.

--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 using namespace std;
 
+int factorial(int a, int d = 1, int k = 1)
+{
+	if (a == k)
+	{
+		return d;
+	}
+	factorial(a, d * k, ++k);
+}
+
+
+
+
 int main()
 {
-
+	int a;
+	cin >> a;
+	cout << factorial(a) << endl;
 	return 0;
 }
