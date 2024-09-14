5debe81c91976a97c1091da0a977460356ad553f
karasb1 <bohdankaras456@gmail.com>
2024-09-14 14:34:20 +0300

    The ex1 was done.

--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
 using namespace std;
 
+int p(int a, int b)
+{
+	for (int i = 0; i < a; i++)
+	{
+		for (int j = 0; j < b; j++)
+		{
+			if (i == 0 || i == a - 1 || j == 0 || j == b - 1)
+			{
+				cout << "* ";
+			}
+			else
+			{
+				cout << "  ";
+			}
+		}
+		cout << endl;
+	}
+	return 0;
+}
+
+
+
+
+
+
+
 int main()
 {
-
-	return 0;
+	int n, k;
+	cin >> n >> k;
+	cout << p(n, k) << endl;
 }
