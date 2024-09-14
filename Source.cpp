0d758fb1a2ad0c2cb5574f5968db2302b9278b4b
karasb1 <bohdankaras456@gmail.com>
2024-09-14 16:40:54 +0300

    The ex4 was done.

--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+int k(int number)
+{
+	return pow(number, 3);
+}
 
 int main()
 {
-
+	int number;
+	cin >> number;
+	cout << k(number) << endl;
 	return 0;
 }
