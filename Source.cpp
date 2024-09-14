3ca301cadd98ba2381cff9744a2bc07679599d10
karasb1 <bohdankaras456@gmail.com>
2024-09-14 14:19:24 +0300

    The ex3 was done.

--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 using namespace std;
 
+int d(int number, int s = 0)
+{
+	for (int i = 1; i < number; i++)
+	{
+		if (number % i == 0)
+		{
+			s += i;
+		}
+	}
+	return s;
+}
+
+
+
+
+
+
 int main()
 {
-
+	int number;
+	cout << "Enter the number: ";
+	cin >> number;
+	cout << ((d(number) == number) ? "true" : "false");
 	return 0;
 }
