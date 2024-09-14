e4b18b3d30727ed1571e568501ac3c653a812be9
karasb1 <bohdankaras456@gmail.com>
2024-09-14 14:43:00 +0300

    The ex3 was done.

--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 using namespace std;
 
+int func(int a, int k = 0)
+{
+	for (int i = 1; i <= a; i++)
+	{
+		if (a % i == 0)
+		{
+			k++;
+		}
+	}
+	return k;
+}
+
+
 int main()
 {
-
+	int number;
+	cin >> number;
+	cout << ((func(number) == 2) ? "true" : "false") << endl;
 	return 0;
 }
